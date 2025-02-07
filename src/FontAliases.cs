c35fd6a2abec7307d850c3f27efac41b42e49bf3
Ibnu Hajar Handityo <andyapin@gmail.com>
2025-02-07 16:44:53 +0700

    support net9.0

--- src/FontAliases.cs
+++ src/FontAliases.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Plugin.Toolkit.Fonts.MaterialIcons
+﻿namespace Plugin.Toolkit.Fonts.MaterialIcons
 {
     internal class FontAliases
     {
